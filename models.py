e4cd181c37d52910aec7b2f690eec88626eb32fc
YWY2002 <wenyuanyik@gmail.com>
2026-09-13 20:49:48 +0800

    transformed continuous for nn training

--- models.py
+++ models.py
@@ -1,2 +1,18 @@
 import torch
+import torch.nn as nn
 import xgboost as xgb
+
+
+class SimpleNet(nn.Module):
+    
+    def __init__(self, input_size, l1, output_size):
+        super(SimpleNet, self).__init__()
+        self.layer1 = nn.Linear(input_size, l1, bias=False, dtype=torch.float16)
+        self.layer2 = nn.Linear(l1, output_size, bias=False, dtype=torch.float16)
+
+    def forward(self, x):
+        y = self.layer1(x)
+        y_hat = torch.relu(y)
+        scores = self.layer2(y_hat)
+        # prob = torch.softmax(scores)
+        return scores
