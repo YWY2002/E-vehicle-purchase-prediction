64f1beca39c852d008ca394c86a30758834ece34
YWY2002 <wenyuanyik@gmail.com>
2026-09-23 12:21:44 +0800

    added mlflow tracking with nn

--- models.py
+++ models.py
@@ -1,18 +1,17 @@
 import torch
 import torch.nn as nn
-import xgboost as xgb
 
 
 class SimpleNet(nn.Module):
     
     def __init__(self, input_size, l1, output_size):
         super(SimpleNet, self).__init__()
-        self.layer1 = nn.Linear(input_size, l1, bias=False, dtype=torch.float16)
-        self.layer2 = nn.Linear(l1, output_size, bias=False, dtype=torch.float16)
+        self.layer1 = nn.Linear(input_size, l1, bias=True, dtype=torch.float32)
+        self.layer2 = nn.Linear(l1, output_size, bias=True, dtype=torch.float32)
 
     def forward(self, x):
         y = self.layer1(x)
         y_hat = torch.relu(y)
         scores = self.layer2(y_hat)
-        # prob = torch.softmax(scores)
+        # prob = torch.softmax(scores, dim=1)
         return scores
